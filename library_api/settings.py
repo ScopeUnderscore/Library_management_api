ef81b54f6454f7ae6faade5135058e63c3649da4
ScopeUnderscore <anandhupraj1279@gmail.com>
2025-01-22 19:55:02 +0530

    Add Author and Book

--- library_api/settings.py
+++ library_api/settings.py
@@ -20,6 +20,7 @@ env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
